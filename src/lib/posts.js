a02367df521ebcfdea9ff80889e6cde1a4fcf0fd
Ajith Renjala <ajithrnayak@icloud.com>
2022-09-06 17:19:57 +0530

    Implemented fetching blog posts

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -5,3 +5,21 @@ import fs from 'fs'
 import { join } from 'path'
 
 const postsDirectory = join(process.cwd(), 'content', 'posts')
+
+  
+export function getAllPosts() {
+    const slugs = fs.readdirSync(postsDirectory)
+    const allPosts = slugs.map((slug) => getPostBySlug(slug))
+  
+    return allPosts
+  }
+  
+export function getPostBySlug(slug) {
+    const realSlug = slug.replace(/\.md$/, '')
+    const fullPath = join(postsDirectory, `${realSlug}.md`)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const { data, content } = matter(fileContents)
+    const date = format(parseISO(data.date), 'MMMM dd, yyyy')
+  
+    return { slug: realSlug, frontmatter: { ...data, date }, content }
+  }
