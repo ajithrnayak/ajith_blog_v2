3f151ef49531c925f3c5e4256bfcd78a10d31caf
Ajith Renjala <ajithrnayak@icloud.com>
2022-09-09 01:24:12 +0530

    Removes console logs used for debugging

--- src/components/image-loader/image-loader.js
+++ src/components/image-loader/image-loader.js
@@ -1,12 +1,9 @@
 import NextImage from 'next/image'
 
 const ImageLoader = ({ src, ...props }) => {
-    console.log("source", src)
-    console.log("props", props)
-
     return (
       <NextImage
-        src={require(`../../../content/posts/${src}`)}
+        src={`/../../../content/posts/${src}`}
         {...props}
         placeholder="blur"
         layout="responsive"
