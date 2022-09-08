77a73e1acd46e8552b050daf6cc7cd97a88db93e
Ajith Renjala <ajithrnayak@icloud.com>
2022-09-08 18:39:39 +0530

    Integrates disqus comments components

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -3,9 +3,14 @@ import Layout from "../components/layout/layout"
 import PostMeta from "../components/post-meta/post-meta"
 import PostContent from "../components/post-content/post-content"
 import BuyMeCoffee from "../components/buymecoffee/buymecoffee"
-
+import { DiscussionEmbed } from "disqus-react"
 
 const Post = ({ content, frontmatter }) => {
+
+  const disqusConfig = {
+    shortname: process.env.GATSBY_DISQUS_NAME,
+    config: { identifier: frontmatter.path },
+  }
 
     return (
       <Layout>
@@ -14,6 +19,7 @@ const Post = ({ content, frontmatter }) => {
         <PostContent content={content} />
         </article>
         <BuyMeCoffee />
+        <DiscussionEmbed {...disqusConfig} />
       </Layout>
     )
   }
